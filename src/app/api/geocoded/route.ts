3613f50b3d58a5ab9b567df4e06e1bcacfd6b34c
matheus <matheus.felipe.19rt@gmail.com>
2024-12-08 16:02:14 -0300

    feat: implement API routes for current & weekly forecasts, air quality, and geocoding

---
+++ src/app/api/geocoded/route.ts
@@ -0,0 +1,18 @@
+import axios from "axios"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(req: NextRequest) {
+	try {
+		const searchParams = req.nextUrl.searchParams
+		const city = searchParams.get("search")
+
+		const url = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=20&language=en&format=json`
+
+		const res = await axios.get(url)
+
+		return NextResponse.json(res.data)
+	} catch (error) {
+		console.log("Error fetching geocoded data")
+		return new Response("Error fetching geocoded data", { status: 500 })
+	}
+}
