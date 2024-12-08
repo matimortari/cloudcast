34cef447712f4391c08bb75fa28a8c0042a8e455
matheus <matheus.felipe.19rt@gmail.com>
2024-12-08 16:11:50 -0300

    refactor: replace axios with fetch for API requests

--- src/app/api/current/route.ts
+++ src/app/api/current/route.ts
@@ -6,14 +6,14 @@ export async function GET(req: NextRequest) {
 		const lat = searchParams.get("lat")
 		const lon = searchParams.get("lon")
 
-		const response = await fetch(
+		const res = await fetch(
 			`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,precipitation,visibility&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&timezone=auto&forecast_days=1`
 		)
 
-		if (!response.ok) {
+		if (!res.ok) {
 			throw new Error("Network response was not ok")
 		}
-		const data = await response.json()
+		const data = await res.json()
 
 		return NextResponse.json(data)
 	} catch (error) {
