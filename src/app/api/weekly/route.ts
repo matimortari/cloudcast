0b568b2b0bda869035570a81fc05748560762176
matheus <matheus.felipe.19rt@gmail.com>
2024-12-08 16:02:14 -0300

    feat(api): set API routes for current & weekly forecasts, air quality, and geocoding

---
+++ src/app/api/weekly/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(req: NextRequest) {
+	try {
+		const searchParams = req.nextUrl.searchParams
+		const lat = searchParams.get("lat")
+		const lon = searchParams.get("lon")
+
+		const response = await fetch(
+			`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,snowfall_sum`
+		)
+
+		if (!response.ok) {
+			throw new Error("Network response was not ok")
+		}
+
+		const data = await response.json()
+
+		return NextResponse.json(data)
+	} catch (error) {
+		console.log("Error fetching weekly forecast data:", error)
+		return new NextResponse("Error fetching weekly forecast data", { status: 500 })
+	}
+}
