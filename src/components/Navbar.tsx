cf3d0ef7666261d82fa78fa07056eded139fe525
matheus <matheus.felipe.19rt@gmail.com>
2024-12-24 11:00:42 -0300

    refactor(css): improve global CSS styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,17 +14,17 @@ export default function Navbar() {
 
 	return (
 		<nav className="mx-4 flex items-center justify-between py-2 lg:mx-8 xl:mx-24">
-			<div className="flex items-center gap-6"></div>
+			<div className="flex items-center gap-6" />
 
 			<div className="flex items-center gap-2">
 				<SearchDialog />
-				<Button onClick={handleThemeToggle} className="button flex h-10 w-10 items-center justify-center">
+				<Button onClick={handleThemeToggle} className="btn flex h-10 w-10 items-center justify-center">
 					<Icon
 						icon={theme === "light" ? "material-symbols:light-mode-rounded" : "material-symbols:dark-mode-rounded"}
 					/>
 				</Button>
-				<a href="https://github.com/matimortari/zephyr" target="_blank" rel="noopener noreferrer">
-					<Button className="button flex items-center">
+				<a href="https://github.com/matimortari/cloudcast" target="_blank" rel="noopener noreferrer">
+					<Button className="btn flex items-center">
 						<Icon icon="simple-icons:github" />
 					</Button>
 				</a>
