17b2be37fee3cc9bb77c40e23602eea6e9ac8fa5
matheus <matheus.felipe.19rt@gmail.com>
2024-12-31 17:17:43 -0300

    feat(seo): add dynamic robots file and new metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,18 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
 	title: "CloudCast 🌤️",
-	description: "Weather App!"
+	description: "Weather App!",
+	keywords: ["CloudCast", "Weather", "Weather App", "CloudCast Page"],
+	openGraph: {
+		url: "https://cloudcast-live.vercel.app/",
+		images: "/cover.png",
+		title: "CloudCast 🌤️",
+		description: "Weather App!",
+		type: "website"
+	},
+	other: {
+		"google-site-verification": "2j0bcfhh8FCYPpzFylzbiPjl3Pa0X7lMuG060ctsCsA"
+	}
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
