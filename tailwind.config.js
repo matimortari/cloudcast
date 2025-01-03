8e4e3056e2889de18fa0295ee9c8c2ea3a79d5c0
matheus <matheus.felipe.19rt@gmail.com>
2025-01-03 01:43:00 -0300

    refactor: cleanup CSS classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,17 +22,20 @@ const config = {
 					DEFAULT: "var(--accent)",
 					foreground: "var(--accent-foreground)"
 				},
-				destructive: {
-					DEFAULT: "var(--destructive)",
-					foreground: "var(--destructive-foreground)"
-				},
 				muted: {
 					DEFAULT: "var(--muted)",
 					foreground: "var(--muted-foreground)"
 				},
 				border: "var(--border)",
 				input: "var(--input)",
-				ring: "var(--ring)"
+				ring: "var(--ring)",
+				chart: {
+					1: "var(--chart-1)",
+					2: "var(--chart-2)",
+					3: "var(--chart-3)",
+					4: "var(--chart-4)",
+					5: "var(--chart-5)"
+				}
 			}
 		}
 	}
