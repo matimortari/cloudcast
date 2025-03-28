d3e24a587151c551c9aa933d03c2cb231443c453
matheus <matheus.felipe.19rt@gmail.com>
2025-03-28 20:12:23 -0300

    refactor(config): update ESLint & Tailwind configs for consistency

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/src/lib/utils"
 import * as React from "react"
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
